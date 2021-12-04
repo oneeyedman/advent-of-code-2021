b7b8b21a1a58416a3ce1a7a94b784d3d146ee0e6
Carlos Mañas <carlos@spacenomads.com>
2021-12-04 13:59:25 +0100

    Add second puzzle of day 3

--- 03/js/main.js
+++ 03/js/main.js
@@ -4,27 +4,28 @@ import { result1, result2, writeResult, getPuzzle } from './dom.js';
 const PUZZLE_URL = 'data/data.txt';
 const PUZZLE_URL_TEST = 'data/test-data.txt';
 
+
 function getMasterBinaryArray(data) {
 	const ones = new Array(data[0].length).fill(0);
-	const zeroes = new Array(data[0].length).fill(0);
+	const zeros = new Array(data[0].length).fill(0);
 
 	for (const bin of data) {
 		for (let i = 0; i < bin.length; i++) {
-			if (bin[i] === 1) { ones[i] += 1; }
-			if (bin[i] === 0) { zeroes[i] += 1; }
+			if (bin[i] === 1) ones[i]++;
+			if (bin[i] === 0) zeros[i]++;
 		}
 	}
 
-	return {ones, zeroes};
+	return {ones, zeros};
 }
 
 
 function getRates(data) {
-	const {ones, zeroes} = data;
+	const {ones, zeros} = getMasterBinaryArray(data);
 	const gamma = [];
 
 	for (let i=0; i<ones.length; i++) {
-		gamma.push(ones[i] >= zeroes[i] ? 1 : 0);
+		gamma.push(ones[i] >= zeros[i] ? 1 : 0);
 	}
 
 	const epsilon = gamma.map(b => b === 1 ? 0 : 1);
@@ -42,22 +43,52 @@ function getRates(data) {
 
 
 function getPowerConsumption(data) {
-
-	const masterBinaryArray = getMasterBinaryArray(data);
-	const rates = getRates(masterBinaryArray);
+	const rates = getRates(data);
 	return rates.gamma.dec * rates.epsilon.dec;
 }
 
 
-async function test(input) {
-	const puzzle = await getPuzzle(input);
+
+function getCommonBit(data, index, value) {
+	let commonBit;
+	const {ones, zeros} = getMasterBinaryArray(data);
+	if (value === 'most') {
+		commonBit = 1;
+		if (zeros[index] > ones[index]) commonBit = 0;
+	} else {
+		commonBit = 0;
+		if (zeros[index] > ones[index]) commonBit = 1;
+	}
+
+	return commonBit;
 }
 
+
+function getRating(data, index = 0, commonValue) {
+	const commonBit = getCommonBit(data, index, commonValue);
+	const filteredData = data.filter(bin => bin[index] === commonBit);
+	if (filteredData.length === 1) {
+		return parseInt(filteredData.flat().join(''), 2);
+	}
+	return getRating(filteredData, index + 1, commonValue);
+}
+
+
+function getLifeSupportRating(data) {
+	const oxigenGeneratorRating = getRating(data, 0, 'most');
+	const co2ScrubberRating = getRating(data, 0, 'least');
+	return oxigenGeneratorRating * co2ScrubberRating;
+}
+
+
+async function test(input) {
+	//const puzzle = await getPuzzle(input);
+}
 
 // test(PUZZLE_URL_TEST);
 // test(PUZZLE_URL);
 
 
 
-writeResult(PUZZLE_URL, result1, getPowerConsumption);
-// writeResult(PUZZLE_URL, result2, multiplyImprovedPositions);
+writeResult(PUZZLE_URL, result1, getPowerConsumption); // 3958484
+writeResult(PUZZLE_URL, result2, getLifeSupportRating); // 1613181
